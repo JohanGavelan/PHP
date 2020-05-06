8f630f7dc48e44b826571dca7d003928c8fe52fe
Johan Gavelan <johangavelan@gmail.com>
2020-05-05 21:14:49 -0500

    Welcome to PHP

--- index.php
+++ index.php
@@ -1,9 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        header {
+            background-color: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
 
-$greeting = 'Hello';
 
-$name = 'Johan Gavelan';
+    <header>
 
-echo "${greeting}, {$name}"
+        <h1>
+            
+            <?=
 
-?>
+            "Hello, " . htmlspecialchars($_GET['name']) .  ". Welcome to PHP";
+
+            ?>
+
+        </h1>
+
+    </header>
+
+
+</body>
+</html>
