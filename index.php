98e93f90a0ecc725728f88ce6d07bbc53ca0a920
Johan Gavelan <johangavelan@gmail.com>
2020-05-08 17:22:32 -0500

    my first function

--- index.php
+++ index.php
@@ -1,23 +1,61 @@
 <?php 
 
 
-$task = [
+// require 'functions.php';
 
-    'title' => 'Finish PHP Class',
 
-    'due' => 'Today',
+// $animals = ['dog', 'cat'];
 
-    'assigned_to' => 'Johan',
 
-    'completed' => false,
-    
-    'restart' => true   
-];
+// function dumper($one, $two, $three) {
 
+//     die(var_dump($one, $two, $three));
+
+// }
 
 
 
-require 'index.view.php';
+// dumper ('hello', 'big' , 'world');
+
+
+function loh($number) {
+
+    if ($number < 10) {
+
+        echo 'the number is less than 10!';
+
+    }
+
+    elseif ($number == 10){
+
+        echo 'the number is 10!';
+
+    }
+
+    else {
+
+        echo 'the number is higher than 10!';
+
+    }
+
+}
+
+
+echo loh(17);
+
+
+
+
+
+
+// echo '<pre>';
+
+// die(var_dump($animals));
+
+// echo '</pre>';
+
+
+// require 'index.view.php';
 
 
 ?>
